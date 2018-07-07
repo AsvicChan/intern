21316f8851702a35a90cffbe3f4c73b5febe6101
Viktor <asvickun@gmail.com>
2018-07-06 17:09:06 -0700

    Removed iostream from class SmartCache

--- SmartCash/SmartCache.h
+++ SmartCash/SmartCache.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "stdafx.h"
-#include <iostream>
 #include <unordered_map>
 
 template <class ID, class T> class SmartCache
@@ -13,32 +12,28 @@ public:
 	template <typename F>
 	SmartCache(F loader) : loader_(loader)
 	{
-		std::cout << "Created smartcache; ";
+		
 	};
 
 	~SmartCache()
 	{
-		std::cout << "Destoyed smartcache. ";
+		
 	};
 
 	std::shared_ptr<T> getPtr(ID id)
 	{
-		std::cout << "getting pointer... ";
 		if (map.end() == map.find(id))
 		{
 			//create pointer
 			map.insert(std::make_pair(id, std::weak_ptr<T>()));
-			std::cout << "pointer created... ";
 		}
 		if (map.at(id).expired() == true)
 		{
 			//load object
 			auto ptr = loader_(id);
 			map.at(id) = ptr;
-			std::cout << "object loaded and pointer sent; ";
 			return map.at(id).lock();
 		}
-		std::cout << "pointer sent; ";
 		return map.at(id).lock();
 	};
 };
