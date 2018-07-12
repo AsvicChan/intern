7b038580142f579f2d715a70473c3adb89316add
Viktor <asvickun@gmail.com>
2018-07-11 19:11:00 -0700

    Added test

--- MinDecorator/MinDecorator/Source.cpp
+++ MinDecorator/MinDecorator/Source.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "MinDecorator.h"
+
+bool filter(int n)
+{
+	if (n % 2 == 0) return true;
+	return false;
+};
 
 int main()
 {
+	int a[10];
+	for (int i = 0; i != 9; ++i)
+	{
+		a[i] = i+1;
+	}
+	int n= *mindecorator(a, a + 8, filter);
+	std::cout << *mindecorator(a, a + 8, filter)<<std::endl;
+	system("pause");
 	return 0;
 }
